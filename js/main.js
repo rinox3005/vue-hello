3c22f39dcbbb15912df26b86489bcae1007ef487
rinox3005 <peppe.rinox@gmail.com>
2024-05-20 18:42:41 +0200

    [BONUS] added img to page from url and a description using data, styled

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ createApp({
   data() {
     return {
       title: "Hello Vue",
+      image:
+        "https://cdn-media-1.freecodecamp.org/images/0*xkJgg-6HskYrQ3N7.jpeg",
+      description:
+        "Vue.js features an incrementally adaptable architecture that focuses on declarative rendering and component composition. The core library is focused on the view layer only. Advanced features required for complex applications such as routing, state management and build tooling are offered via officially maintained supporting libraries and packages",
     };
   },
 }).mount("#vue-try");
